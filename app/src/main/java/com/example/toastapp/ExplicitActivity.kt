b0be12a728a53c5c167b9d4bb35367a3976045ac
[shubhamsingh74] <shubhamsingh74970@gmail.com>
2023-02-26 01:28:13 +0530

    hello done

--- app/src/main/java/com/example/toastapp/ExplicitActivity.kt
+++ app/src/main/java/com/example/toastapp/ExplicitActivity.kt
@@ -1,11 +1,45 @@
 package com.example.toastapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
 
 class ExplicitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explicit)
+
+        val kcall = findViewById<Button>(R.id.call)
+        val klinkedin = findViewById<Button>(R.id.linkedin)
+        val kgithub= findViewById<Button>(R.id.github)
+        val ktwitter = findViewById<Button>(R.id.twitter)
+
+
+        klinkedin.setOnClickListener{
+            val intent= Intent(Intent.ACTION_VIEW)
+            intent.data= Uri.parse("https://www.linkedin.com/in/shubhamsingh2135/")
+            startActivity(intent)
+        }
+
+        kgithub.setOnClickListener{
+            val intent= Intent(Intent.ACTION_VIEW)
+            intent.data= Uri.parse("https://github.com/shubhamsingh74")
+            startActivity(intent)
+        }
+
+        ktwitter.setOnClickListener{
+            val intent= Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+//            intent.data= Uri.parse("https://twitter.com/devsinghshubham")
+            startActivity(intent)
+        }
+
+        kcall.setOnClickListener{
+            val intent= Intent(Intent.ACTION_CALL)
+            intent.data= Uri.parse("tel: 7027076155")
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/toastapp/MainActivity.kt
+++ app/src/main/java/com/example/toastapp/MainActivity.kt
@@ -2,38 +2,56 @@ package com.example.toastapp
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("ResourceAsColor")
+    @SuppressLint("ResourceAsColor", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val callshubham = findViewById<Button>(R.id.shubham)
-        val callsingh = findViewById<Button>(R.id.singh)
-        val callbutton= findViewById<Button>(R.id.buttonshubham)
 
-        callbutton.setOnClickListener{
-            Toast.makeText(applicationContext,"Hello Button",Toast.LENGTH_SHORT).show()
-            callbutton.setBackgroundColor(R.color.black)
+        val butto = findViewById<Button>(R.id.Button)
+//        val kcall = findViewById<Button>(R.id.call)
+//        val klinkedin = findViewById<Button>(R.id.linkedin)
+//        val kgithub= findViewById<Button>(R.id.github)
+//        val ktwitter = findViewById<Button>(R.id.twitter)
+//
+//
+//        klinkedin.setOnClickListener{
+//            val intent=Intent(Intent.ACTION_VIEW)
+//            intent.data= Uri.parse("https://www.linkedin.com/in/shubhamsingh2135/")
+//            startActivity(intent)
+//        }
+//
+//        kgithub.setOnClickListener{
+//            val intent=Intent(Intent.ACTION_VIEW)
+//            intent.data= Uri.parse("https://github.com/shubhamsingh74")
+//            startActivity(intent)
+//        }
+//
+//        ktwitter.setOnClickListener{
+//            val intent=Intent(Intent.ACTION_VIEW)
+//            intent.data= Uri.parse("https://twitter.com/devsinghshubham")
+//            startActivity(intent)
+//        }
+//
+//        kcall.setOnClickListener{
+//            val intent=Intent(Intent.ACTION_CALL)
+//            intent.data= Uri.parse("7027076155")
+//            startActivity(intent)
+//        }
 
-
-        }
-
-
-
-        callshubham.setOnClickListener {
-//            Toast.makeText(applicationContext, "Hello Shubham...", Toast.LENGTH_LONG).show()
+        butto.setOnClickListener {
+           Toast.makeText(applicationContext, "Hello Shubham...", Toast.LENGTH_LONG).show()
             intent = Intent(applicationContext, ExplicitActivity::class.java)
             startActivity(intent)
         }
-        callsingh.setOnClickListener {
-            Toast.makeText(applicationContext, "Hello Singh...", Toast.LENGTH_LONG).show()
-        }
+
 
     }
 }
